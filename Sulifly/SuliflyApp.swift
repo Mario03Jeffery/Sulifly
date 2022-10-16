f708da3ef276255417f9c7b58a54c726daad4901
Mario <marioehiagwina03@icloud.com>
2022-10-16 19:49:01 +0200

    Initial Commit

---
+++ Sulifly/SuliflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuliflyApp.swift
+//  Sulifly
+//
+//  Created by Mario Ehiagwina on 2022-10-16.
+//
+
+import SwiftUI
+
+@main
+struct SuliflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
